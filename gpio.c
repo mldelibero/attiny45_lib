ec5b35c4673d175d07479b73ae81d5518f468cdb
Michael DeLibero <mldelibero@new-host.home>
2013-10-20 17:10:19 -0400

    Initial versions of gpio libraries

---
+++ gpio.c
@@ -0,0 +1,21 @@
+#include "gpio.h"
+
+void port_setDir(uint8_t pins) {
+	DDRB = pins;
+}
+
+void setPins(uint8_t pins) {
+	PORTB |= pins;
+}
+
+void clearPins(uint8_t pins) {
+	PORTB |= !pins;
+}
+
+uint8_t getPortVal(void) {
+	return PINB;
+}
+
+uint8_t getPinVal(uint8_t) {
+	return PINB & pin;
+}
---
+++ gpio.h
@@ -0,0 +1,10 @@
+#ifndef __GPIO_H
+#define __GPIO_H
+
+void port_setDir(uint8_t pins);
+void setPins(uint8_t pins);
+void clearPins(uint8_t pins);
+uint8_t getPortVal(void);
+uint8_t getPinVal(uint8_t);
+
+#endif // __GPIO_H
